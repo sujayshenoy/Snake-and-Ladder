11c847b9f8444929ff8d7c2bb46dd7d736939fd9
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-08-23 21:01:47 +0530

    Methods added to check if position is ladder, snake or no-play

--- src/com/snakeandladder/Main.java
+++ src/com/snakeandladder/Main.java
@@ -10,13 +10,37 @@ public class Main {
 		
 		//new game with initial position 0 is created
 		Game g = new Game();
-		System.out.println("Die Says: "+g.rollDie());
+		
+		System.out.println("Press Enter to roll the die");
+		System.out.println("'STOP' to stop playing");
+		Scanner s = new Scanner(System.in);
+		
+		while(true) {
+			String resp = s.nextLine();
+			if(resp.equals("STOP")) {
+				System.out.println("Stopping Game\nGame Terminated Successfully");
+				break;
+			}
+			else {
+				if(g.noPlayFlag == 0) {
+					g.play();
+				}
+				else {
+					System.out.println("You cannot play this turn");
+					g.noPlayFlag = 0;
+				}
+			}
+			System.out.println("Press Enter to roll the die again");
+			System.out.println("'STOP' to stop playing");
+		}
+		
 	}
 	
 }
 
 class Game{
 	int position = 0;
+	int noPlayFlag = 0;
 	
 	//Positions of Snakes and Ladders
 	Map<Integer,Integer> ladders = Map.ofEntries(
@@ -41,8 +65,79 @@ class Game{
 										entry(98,79)
 									);
 	
+	List<Integer> noPlay = List.of(3,13,25,37,41,61,74,97);
+	
 	public int rollDie() {
-		return (int)Math.floor(Math.random()*((6-1)+1)+1);
+		int numberOnDie = (int)Math.floor(Math.random()*((6-1)+1)+1);
+		System.out.println("Die Says: "+numberOnDie);
+		return numberOnDie;
+	}
+	
+	public int checkLadder(int pos) {
+		
+		for(Integer item : ladders.keySet()) {
+			if(pos == item) {
+				System.out.println("Ladder Hit");
+				pos = ladders.get(item);
+				return pos;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int checkSnake(int pos) {
+		
+		for(Integer item : snakes.keySet()) {
+			if(pos == item) {
+				System.out.println("Snake Hit");
+				pos = snakes.get(item);
+				return pos;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public int checkNoPlay(int pos) {
+		
+		for(Integer item : noPlay) {
+			if(pos == item) {
+				System.out.println("No Play");
+				noPlayFlag = 1;
+				return pos;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public void play() {
+		
+		int dieNumber = rollDie();
+		
+		int ladderPos = checkLadder(position+dieNumber);
+		int snakePos = checkSnake(position+dieNumber);
+		int noPlayPos = checkNoPlay(position+dieNumber);
+		if( ladderPos > 0) {
+			position = ladderPos;
+		}
+		else if ( snakePos > 0 ){
+			position = snakePos;
+		}
+		else if (noPlayPos > 0){
+			position = noPlayPos;				
+		}
+		else {
+			position += dieNumber;
+		}
+		
+		printCurrentPosition();
+	
+	}
+	
+	public void printCurrentPosition() {
+		System.out.println("Position: "+position);
 	}
 	
 }
